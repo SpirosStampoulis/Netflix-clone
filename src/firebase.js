adbe78f9dd9918a32fc6fae10b30a83a0a75212d
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-06 10:13:36 +0100

    Add screens

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2u_W6jTbqP6YOmkI0epnYhXEWRlqsjA8",
+    authDomain: "netflix-clone-35e37.firebaseapp.com",
+    projectId: "netflix-clone-35e37",
+    storageBucket: "netflix-clone-35e37.appspot.com",
+    messagingSenderId: "42185662773",
+    appId: "1:42185662773:web:d92f14c07ad00831ce6c7c"
+  };
